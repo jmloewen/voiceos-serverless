d3527afabcaa5a81590a1c097bf1f451276cba38
jmloewen <jmloewen@sfu.ca>
2018-03-11 17:29:18 -0700

    pushing simple get/post request demo code.  need an aws access / secret key for this to work.

---
+++ handler.py
@@ -0,0 +1,38 @@
+import json
+import datetime
+#from SimpleHTTPServer import SimpleHTTPRequestHandler
+#import SocketHandler
+
+def endpoint(event, context):
+    current_time = datetime.datetime.now().time()
+
+    if str(event['requestContext']['httpMethod']) == "GET":
+        body = {
+            "request type": "" + str(event['requestContext']['httpMethod'])
+        }
+    elif str(event['requestContext']['httpMethod']) == "POST":
+        body = {
+            "request type": "" + str(event['requestContext']['httpMethod'])
+        }
+    else:
+        body={
+            "request type": "Neither GET or POST"
+        }
+
+
+    #body = {
+    #    "heres a header!"
+    #}
+    #else:
+    #    body = "request type": "" + str(event['requestContext']['httpMethod'])
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+    return response
+
+#class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+#    def do_POST(self):
+#        self._set_headers()
+#        self.wfile.write("<html><head><title>POST TITLE</title></head><body><h1>POSTING to serverless ep</h1></body></html>")
