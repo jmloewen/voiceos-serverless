4a1539aa507bafbdc7329a7f82db2c492ee783c1
June <kimjune01@gmail.com>
2018-04-01 12:41:44 -0700

    Revert "refactor handler to be more descriptive"
    This reverts commit fece0c8a78bb0d91d7ce3332212c1ffbffa36100.

--- handler.py
+++ handler.py
@@ -1,37 +1,54 @@
 import json
 import datetime
 import requests
-
+#from SimpleHTTPServer import SimpleHTTPRequestHandler
+#import SocketHandler
 RASASERVER_URL = "http://ec2-34-218-219-244.us-west-2.compute.amazonaws.com:5000/parse"
 
-def isPostRequest(event):
-    return str(event['requestContext']['httpMethod']) == "POST"
-
-def unwrapEvent(event):
-    wrappedPayload = json.loads(event['body'])
-    return wrappedPayload['payload'], wrappedPayload['sender']
-
-def postRasaForIntent(payload):
-    r = requests.post(RASASERVER_URL, json={"q": payload})
-    return r.json()
-
-def intentNameFromRasaJson(json):
-    return rasaResponse['intent']['name']
-
-def wrapIntentSpeakAction(json, sender):
-    bodyPayload = {
-        "actionType": "speak",
-        "actionDetail": intentNameFromRasaJson(json)
-    }
-    body = {
-        "receiver": sender, # always send back to the sender for now...
-        "payload": bodyPayload
-    }
-    return { "statusCode": 200, "body": json.dumps(body) }
-
 def endpoint(event, context):
-    if !isPostRequest(event):
-        return { "statusCode": 422, "body": "Request should be POST"}
-    payload, sender = unwrapEvent(event)
-    rasaJson = postRasaForIntent(payload)
-    return wrapIntentSpeakAction(rasaJson, sender)
+    current_time = datetime.datetime.now().time()
+
+    if str(event['requestContext']['httpMethod']) == "GET":
+        body = {
+            "request type": "" + str(event)
+        }
+    elif str(event['requestContext']['httpMethod']) == "POST":
+
+        wrappedPayload = json.loads(event['body'])
+        print("wrappedPayload: ", wrappedPayload)
+        payload = wrappedPayload['payload']
+        r = requests.post(RASASERVER_URL, json={"q": payload})
+        response = r.json()
+        intentString = response['intent']['name']
+        responsePayload = {
+            "actionType": "speak",
+            "actionDetail": intentString
+        }
+        body = {
+            "receiver": wrappedPayload["sender"],
+            "payload": responsePayload
+        }
+
+
+    else:
+        body={
+            "request type": "Neither GET or POST"
+        }
+
+
+    #body = {
+    #    "heres a header!"
+    #}
+    #else:
+    #    body = "request type": "" + str(event['requestContext']['httpMethod'])
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+    return response
+
+#class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+#    def do_POST(self):
+#        self._set_headers()
+#        self.wfile.write("<html><head><title>POST TITLE</title></head><body><h1>POSTING to serverless ep</h1></body></html>")
